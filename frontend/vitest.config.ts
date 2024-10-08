d66e459ad5c2faa935311b28e7d54e3bc7edac22
Synne Vineshaugen Raad <synnevr@stud.ntnu.no>
2024-10-08 12:58:38 +0200

    GH-44 Added coverage testing with Istanbul
    Run npx vitest --coverage when all testing is done.

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/setupTests.ts",
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });
