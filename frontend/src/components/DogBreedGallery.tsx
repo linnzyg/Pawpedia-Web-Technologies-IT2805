4b77a7726cd187e5d085c3de1be8537788e570a9
Linn Zhu Yu Grotnes <lzgrotne@stud.ntnu.no>
2024-10-26 21:25:05 +0200

    Import shad cn ui and style unfinished
    Import shad cn ui components
    Make the site more accessible and clean
    Pushing to be able to merge with new changes

--- frontend/src/components/DogBreedGallery.tsx
+++ frontend/src/components/DogBreedGallery.tsx
@@ -4,6 +4,15 @@ import '../style/SortOrFilter.css';
 import { useQuery } from '@apollo/client';
 import { GET_BREEDS } from '../api/queries';
 import { DogBreed } from '../types/DogBreed';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Menubar
+} from "@/components/ui/menubar"
 
 const DogBreedGallery: React.FC = () => {
   const { loading, error, data } = useQuery<{ breeds: DogBreed[] }>(GET_BREEDS);
@@ -67,11 +76,12 @@ const DogBreedGallery: React.FC = () => {
 
   return (
     <>
-      <header id="sortOrFilter">
+    <section id="sortOrFilter">
+      <section id="firstRow">
         <label htmlFor="sort">Sort by</label>
         <select ref={sortRef} name="sort" id="sort" onChange={optionClicked} defaultValue="chooseSorting">
           <option value="chooseSorting" disabled>
-            Choose...
+          Choose...
           </option>
           <option value="alpha">Alphabetically</option>
           <option value="favorites">Favorites</option>
@@ -84,25 +94,34 @@ const DogBreedGallery: React.FC = () => {
           <option value="bigDogs">Big dogs</option>
           <option value="smallDogs">Small dogs</option>
         </select>
+      </section>
+
+      <section id="secondRow">
         <input type="text" placeholder="Search..." value={searchQuery} onChange={handleSearchChange} />
         <button id="reset-btn" onClick={resetFiltersAndSorting}>
           Reset
         </button>
-      </header>
-      <div className="dog-breed-gallery">
+      </section>
+    </section>
+
+      <section className="dog-breed-gallery">
         {sortedDogs.length > 0 ? (
           sortedDogs.map((breed) => (
-            <div key={breed.id} className="breed-card">
-              <Link to={`/${breed.id}`}>
-                <h2>{breed.name}</h2>
-                <img src={`/images/${breed.image}`} alt={`Picture of our dog breed: ${breed.name}`} />
-              </Link>
-            </div>
+            <Link to={`/${breed.id}`}> 
+              <Card key={breed.id} className="breed-card" >
+                <CardTitle>
+                  <p>{breed.name}</p>
+                </CardTitle>
+                <CardContent>
+                  <img src={`/images/${breed.image}`} alt={`Picture of a ${breed.name}`} />
+                </CardContent>
+              </Card>
+            </Link>
           ))
         ) : (
           <p>No breeds found.</p>
         )}
-      </div>
+      </section>
     </>
   );
 };
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -6,34 +6,35 @@ function Home() {
     <>
       <section id="homeBody">
         <header>
-          <h1>Welcome to the dog breeds database!</h1>
+          <h1>Welcome to Pawpedia, a dog breed database!</h1>
         </header>
 
         <section>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at nibh nec augue vehicula mollis id et est. Cras
-          lacus ex, maximus nec tellus vitae, aliquam facilisis velit. Sed aliquam a arcu scelerisque pharetra. Nunc
-          quis venenatis ex. Aliquam dapibus venenatis rutrum. Cras maximus magna mauris. Pellentesque et leo sem.
-          Aenean sit amet laoreet nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur felis
-          purus, placerat nec dapibus quis, tristique a massa. Vivamus consequat metus id ex egestas feugiat. Nullam sed
-          dui ac enim iaculis sodales. Vestibulum venenatis massa id consequat ultrices. Donec rhoncus justo eu felis
-          sagittis, sed pellentesque nisl sagittis.
-          <br />
-          <br />
-          Nulla non mauris aliquet, sagittis urna a, imperdiet ex. Donec convallis sollicitudin purus, nec condimentum
-          quam pretium ut. Mauris venenatis leo at neque consequat rhoncus. Maecenas quis imperdiet tellus, eget mollis
-          elit. Proin quis hendrerit eros. Aenean nec elit ac metus sollicitudin consequat. Integer condimentum bibendum
-          velit, ac euismod lectus varius et. Curabitur fringilla, lectus eget blandit porta, ipsum diam varius nibh,
-          quis molestie sem sapien ornare justo. Quisque lobortis urna sit amet dui feugiat, sed pharetra leo congue.
-          Maecenas a felis neque. Etiam magna purus, tempor at orci id, tempus pellentesque tellus. Sed at ornare magna.
-          Donec elementum, mi non vehicula commodo, nulla elit aliquam lectus, quis tristique augue nisi porta nisl.
-          <br />
-          <br />
-          Phasellus suscipit tortor non commodo egestas. Proin iaculis dapibus leo at dignissim. Aliquam faucibus ex
-          nisl, sit amet tempor lectus porta quis. Suspendisse ut lorem semper, rutrum elit eu, vehicula justo. Duis
-          volutpat maximus interdum. Aliquam dapibus nisl dapibus, posuere mauris nec, pretium eros. Pellentesque sit
-          amet gravida risus, accumsan euismod erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          pellentesque suscipit dictum. Nam interdum vulputate mattis. Duis cursus erat a elit auctor gravida. Ut sit
-          amet ipsum sollicitudin, pretium nisl ut, luctus turpis.
+        <p>
+          Pawpedia is an application designed to showcase various dog breeds. The first version uses static mocked data to display dog breeds in a gallery view. The gallery view can lead the user to the detailed view about each dog breed by pressing the pictures of the dogs.
+        </p>
+        <p>
+          The coming versions of Pawpedia will contain a database running a backend meaning more data and dog breeds. Each breed will also have more details and information about the breed itself. Styling will also be done using third party libraries such as shadcn.
+        </p>
+        
+        <p>Features:</p>
+        <ul>
+          <li>
+            Gallery View: Browse all dog breeds in an easy-to-navigate gallery
+          </li>
+          <li>
+            Detailed Breed View: Click on a breed to view more detailed information
+          </li>
+          <li>
+            Sorting Options: Sort breeds alphabetically or by your favorites
+          </li>
+          <li>
+            Filters: Filter breeds by size—small or large dogs
+          </li>
+          <li>
+            Search: Quickly search for dog breeds by name
+          </li>
+        </ul>
         </section>
 
         <footer>Contact information: 98765432</footer>
