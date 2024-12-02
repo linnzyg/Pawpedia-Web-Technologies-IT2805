fc8d02515ba85e840c11d59f179ffedbe0183382
Linn Zhu Yu Grotnes <lzgrotne@stud.ntnu.no>
2024-12-02 18:13:09 +0100

    Make DogCard component
    - made DogCard.tsx
    - styling changes on dogcard

--- frontend/src/components/FavoritesGrid.tsx
+++ frontend/src/components/FavoritesGrid.tsx
@@ -5,8 +5,9 @@ import '../style/FavoritesGrid.css';
 import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
 import '../style/DogCard.css';
+import DogCard from './DogCard';
 
-type FavoriteBreed = Pick<DogBreed, 'id' | 'name' | 'image'>;
+type FavoriteBreed = Pick<DogBreed, 'id' | 'name' | 'image' | 'averageRating'>;
 
 function FavoritesGrid() {
   const [favorites, setFavorites] = useState<FavoriteBreed[]>([]);
@@ -15,7 +16,12 @@ function FavoritesGrid() {
     const storedFavorites = localStorage.getItem('favorites');
     if (storedFavorites) {
       const parsedFavorites: DogBreed[] = JSON.parse(storedFavorites);
-      const limitedFavorites = parsedFavorites.map(({ id, name, image }) => ({ id, name, image }));
+      const limitedFavorites = parsedFavorites.map(({ id, name, image, averageRating }) => ({
+        id,
+        name,
+        image,
+        averageRating,
+      }));
       setFavorites(limitedFavorites);
     }
   };
@@ -33,14 +39,7 @@ function FavoritesGrid() {
       </header>
       <Box className="favorites-grid">
         {favorites.length > 0 ? (
-          favorites.map((breed) => (
-            <Card key={breed.id} className="breed-card">
-              <Link to={`/${breed.id}`}>
-                <h2>{breed.name}</h2>
-                <img src={`/images/${breed.image}`} alt={`Picture of ${breed.name}`} />
-              </Link>
-            </Card>
-          ))
+          favorites.map((breed) => <DogCard key={breed.id} breed={breed} />)
         ) : (
           <p>No breeds found.</p>
         )}
