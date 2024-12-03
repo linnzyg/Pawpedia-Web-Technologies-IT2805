bd409223425b7bfac1f2e1e5ab0eafc5663ba342
Synne Vineshaugen Raad <synnevr@stud.ntnu.no>
2024-12-03 15:47:52 +0100

    GH-151 Remove unused/replaced fetching favorite functionality

--- frontend/src/utils/randomBreedFetcher.ts
+++
@@ -1,9 +0,0 @@
-type BreedNode = {
-  node: { id: string };
-};
-
-export const getRandomBreedId = (edges: BreedNode[]) => {
-  if (edges.length === 0) return null;
-  const randomIndex = Math.floor(Math.random() * edges.length);
-  return edges[randomIndex].node.id;
-};
