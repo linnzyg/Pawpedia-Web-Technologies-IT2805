28bdf7263366c17854d08d1ab038e6534242062a
Linn Zhu Yu Grotnes <lzgrotne@stud.ntnu.no>
2024-12-05 23:22:20 +0100

    GH-198 Add things for vm

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,15 +8,17 @@ import { Provider } from 'react-redux';
 import store from './components/redux/store';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000', // Replace with VM address later
+  uri: 'http://it2810-35.idi.ntnu.no:3001',
   cache: new InMemoryCache(),
 });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      {' '}
+      {}
       <ApolloProvider client={client}>
-        <BrowserRouter>
+        <BrowserRouter basename='/project2'>
           <App />
         </BrowserRouter>
       </ApolloProvider>
