c9af39e3c994bb5272dde7921be3c52724e2d5d9
Linn Zhu Yu Grotnes <lzgrotne@stud.ntnu.no>
2024-12-06 22:10:00 +0100

    Update SizeFiltering.tsx

--- frontend/src/components/AllDogsPage/SortFilterOrSearch/SizeFiltering.tsx
+++ frontend/src/components/AllDogsPage/SortFilterOrSearch/SizeFiltering.tsx
@@ -24,7 +24,7 @@ const SizeFiltering: React.FC<FilteringProps> = ({ onFilterChange, filterBySize 
       onFilterChange(selectedSizes);
       isFirstRender.current = false;
     }
-  }, [onFilterChange, selectedSizes]);
+  }, [selectedSizes]);
 
   const sizes = ['Small', 'Medium', 'Large', 'Giant'];
 
@@ -39,7 +39,7 @@ const SizeFiltering: React.FC<FilteringProps> = ({ onFilterChange, filterBySize 
         labelId="size-filter-label"
         multiple
         value={selectedSizes}
-        //@ts-expect-error - onChange type error
+        //@ts-ignore
         onChange={handleSizeChange}
         renderValue={(selected) => (selected as string[]).join(', ')} //Show selected sizes
         sx={{ width: '100%' }}
