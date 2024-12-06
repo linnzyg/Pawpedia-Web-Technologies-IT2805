a3ce9589e44f7349d8d3f06a3c998842fbbd890d
Synne Vineshaugen Raad <synnevr@stud.ntnu.no>
2024-12-06 18:50:45 +0100

    GH-248 Fix one merge conflict
    Everything else is good!

--- frontend/src/components/redux/reducers/searchReducer.ts
+++ frontend/src/components/redux/reducers/searchReducer.ts
@@ -1,16 +1,8 @@
-
-<<<<<<< Updated upstream
-const initialState = '';
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const searchReducer = (state = initialState, action: any) => {
-=======
-
 import { SET_SEARCH, Action } from '../actions/types';
 
 const initialState: string = '';
 
 const searchReducer = (state = initialState, action: Action): string => {
->>>>>>> Stashed changes
   switch (action.type) {
     case SET_SEARCH:
       return action.payload;
